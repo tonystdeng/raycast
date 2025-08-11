84122ca490fd16444fe020d0aa9de2c7186e87ef
tonystdeng <tony.st.deng@gmail.com>
2025-08-11 05:21:04 -0400

    auto commit idk why i'm just a script named git.bash trying to do my job bruh

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
-sudo pacman -Syu sfml
+# sudo pacman -Syu sfml
 
 g++ -c ./src/main.cpp ./src/loadMap.cpp ./src/logic.cpp ./src/sfmlDisplay.cpp ./src/terminal.cpp
 g++ ./main.o ./loadMap.o ./logic.o ./sfmlDisplay.o ./terminal.o -o mainExecutable  \
@@ -13,4 +13,4 @@ mv mainExecutable bin
 cp /usr/lib/libsfml-graphics.so* /usr/lib/libsfml-window.so* /usr/lib/libsfml-system.so* bin/
 sudo pacman -Rns sfml
 
-bash raycast-app.bash
+# bash raycast-app.bash
